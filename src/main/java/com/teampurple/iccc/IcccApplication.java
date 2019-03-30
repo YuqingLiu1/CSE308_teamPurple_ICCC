96ec623cd138e3f715edecdc1df306c3b5b7ae1d
Elliot Tuck <eat3774@gmail.com>
2019-03-30 15:52:48 -0400

    Initial commit

---
+++ src/main/java/com/teampurple/iccc/IcccApplication.java
@@ -0,0 +1,13 @@
+package com.teampurple.iccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teampurple/iccc/IcccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teampurple.iccc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IcccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

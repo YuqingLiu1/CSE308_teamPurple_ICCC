b83f9f4769e7e24340c57371627435751b18dc69
Yuqing <yuqing.liu@stonybrook.edu>
2019-04-09 20:08:45 -0400

    Add category

--- src/main/java/com/teampurple/iccc/models/Category.java
+++ src/main/java/com/teampurple/iccc/models/Category.java
@@ -1,5 +1,48 @@
 package com.teampurple.iccc.models;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.*;
+
+@Document(collection = "Category")
 public class Category {
+    @Id
+    private ObjectId id;
+    private ObjectId user;
+    private String name;
+    private List<String> filters;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public ObjectId getUser() {
+        return user;
+    }
+
+    public void setUser(ObjectId user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getFilters() {
+        return filters;
+    }
+
+    public void setFilters(List<String> filters) {
+        this.filters = filters;
+    }
 
 }

26d9d9c9cc28263ef7a8a9536a359b6c542952dd
Elliot Tuck <eat3774@gmail.com>
2019-04-13 17:12:22 -0400

    Make development easier
    Configure webpack dev server so that live-reloading is available to
    quickly develop the React portion of the project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 var path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/main/js/app.js'
     },
     devtool: 'sourcemaps',
-    cache: true,
-    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'src', 'main', 'resources', 'static', 'built'),
+        publicPath: '/built/',
+        proxy: {
+            '/': 'http://localhost:80'
+        }
+    },
     output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        path: path.join(__dirname, 'src', 'main', 'resources', 'static', 'built'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"]
+                        }
                     }
-                }]
+                ]
             }
         ]
-    }
+    },
 };

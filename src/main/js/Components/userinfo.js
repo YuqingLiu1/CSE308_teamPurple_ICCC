40322215fedaa1345928ebaaf9f1fcc9b743d760
Elliot Tuck <eat3774@gmail.com>
2019-04-17 00:24:11 -0400

    Add DBAwareEdiText component
    Add an EdiText component that is aware of the database and automatically
    saves changes there. In addition to specifying typical EdiText props,
    you now have to specify the name of the field (in UserInfo) that you
    want the text to be saved to.

--- src/main/js/Components/userinfo.js
+++ src/main/js/Components/userinfo.js
@@ -6,54 +6,16 @@ import Container from 'react-bootstrap/Container';
 import Thumbnail from './thumbnail';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import EdiText from "react-editext";
+import DBAwareEdiText from "./DBAwareEdiText";
 
 class UserInfo extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            error: false,
-            username: '',
-            bio: ''
-        }
-
-        this.onSave = this.onSave.bind(this);
+            error: false
+        };
     }
-
-    async componentDidMount() {
-        let userInfoRes = await fetch('/user/info');
-        userInfoRes = await userInfoRes.json();
-        if (userInfoRes.status === 'OK') {
-            let userInfo = userInfoRes.content;
-            this.setState({
-                bio: userInfo.bio,
-                username: userInfo.username
-            });
-        } else {
-            this.setState({
-                error: true
-            });
-        }
-    }
-
-    async onSave(val) {
-        let userInfoRes = await fetch('/user/edit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {
-                bio: val
-            }
-        });
-        userInfoRes = await userInfoRes.json();
-        if (userInfoRes.status === 'OK') {
-            // do something on good save
-        } else {
-            // do something on bad save
-        }
-    };
 
     render() {
         if (this.state.error) {
@@ -75,13 +37,13 @@ class UserInfo extends Component {
                         <Container>
                             <Row>
                                 <Col xs="{3}">
-                                    <Thumbnail src={this.props.imageUrl} title={this.state.username}/>
+                                    <Thumbnail src={this.props.imageUrl} title={this.props.username}/>
                                 </Col>
                                 <Col>
-                                    <EdiText
+                                    <DBAwareEdiText
                                         type="textarea"
-                                        value={this.state.bio}
-                                        onSave={this.onSave}
+                                        name="bio"
+                                        value={this.props.bio}
                                     />
                                 </Col>
                             </Row>

a46028954c5a3e6cc18c4ecfcfbdc5473ce16d49
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-18 23:17:40 -0400

    Added hearts

--- src/main/js/Components/Likes.js
+++ src/main/js/Components/Likes.js
@@ -1,1 +1,29 @@
-import
+import React, {Component} from 'react'
+import HeartCheckbox from 'react-heart-checkbox';
+
+class Demo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			checked: false
+		};
+	}
+
+	onClick = (evnet, props) => {
+		this.setState({ checked: !this.state.checked });
+	}
+
+	render() {
+		const { checked } = this.state;
+
+		return (
+			<>
+				<h1>{checked ? 'checked' : 'unchecked'}</h1>
+				<HeartCheckbox checked={checked} onClick={this.onClick} />
+			</>
+		);
+	}
+}
+
+// render(<Demo />, document.getElementById("app"));
+return Demo

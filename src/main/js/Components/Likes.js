2eb77e5757ea754cbf2d1049ae2a68230752ac13
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-19 03:56:19 -0400

    Added ArrowButton.js

--- src/main/js/Components/Likes.js
+++ src/main/js/Components/Likes.js
@@ -3,12 +3,23 @@ import Heart from './Heart'
 export default function({generalBaseId='Stub', likedByCurrentUserColor='red', notLikedByCurrentUserColor='black'})
 {
 	const [likedByCurrentUser, setLikedByCurrentUser]=useState(true)//This should fetch whether a user likedByCurrentUser this or not
-	console.assert(likedByCurrentUser!==undefined&&Object.getPrototypeOf(likedByCurrentUser)===Boolean.prototype)//Type checking that likedByCurrentUser is boolean
-	return <div style={{
-		backgroundColor: "Green",
-		height         : "300px",
-		width          : "300px"
-	}}>
-		<Heart/>
+	console.assert(likedByCurrentUser!==undefined && Object.getPrototypeOf(likedByCurrentUser)===Boolean.prototype)//Type checking that likedByCurrentUser is boolean
+	function toggleLikedByCurrentUser()
+	{
+		setLikedByCurrentUser(!likedByCurrentUser)
+	}
+	//
+	return <div
+		style={{
+			backgroundColor: "rgba(1,1,1,.2)",
+			// height         : "30px",
+			padding:"10px",
+			borderRadius:"10px",
+			// width          : "30px"
+		}}>
+		<Heart style={{height:"30px"}}
+				onClick={toggleLikedByCurrentUser}
+			   color={likedByCurrentUser ? likedByCurrentUserColor : notLikedByCurrentUserColor}/>
+		12389
 	</div>
 }
--- src/main/js/Components/ProfileCard.js
+++ src/main/js/Components/ProfileCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from "react-bootstrap/Image";
 import DBAwareEdiText from "./DBAwareEdiText";
+import Likes from './Likes'
 
 export default function({profileThumbnailUrl, username}) {
     return <div>
@@ -14,5 +15,6 @@ export default function({profileThumbnailUrl, username}) {
                 />
             </div>
         </div>
+        <Likes/>
     </div>
 }

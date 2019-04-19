a3abc1ca9932200f014f024d53947fc66b938ad3
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-18 23:57:41 -0400

    Fix

--- src/main/js/Components/Likes.js
+++ src/main/js/Components/Likes.js
@@ -1,28 +1,8 @@
-import React, {Component} from 'react'
-import HeartCheckbox from 'react-heart-checkbox';
-
-class Demo extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			checked: false
-		};
-	}
-
-	onClick = (evnet, props) => {
-		this.setState({ checked: !this.state.checked });
-	}
-
-	render() {
-		const { checked } = this.state;
-
-		return (
-			<>
-				<h1>{checked ? 'checked' : 'unchecked'}</h1>
-				<HeartCheckbox checked={checked} onClick={this.onClick} />
-			</>
-		);
-	}
+import React from 'react'
+import Heart from './Heart'
+export default function()
+{
+	return <div style={{backgroundColor:"Green",height:"300px",width:"300px"}}>
+		<Heart/>
+	</div>
 }
-
-export default Demo

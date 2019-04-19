1b39ac6084b89a14c459cbb907536bedf8730475
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-18 23:26:17 -0400

    comit

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,87 +1,71 @@
 require("@babel/polyfill")
 
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 
-import Menubar from './Components/menubar';
-import UserInfo from "./Components/userinfo";
-import LoggedOutCategories from './Components/loggedOutCategories';
-import LoggedInCategories from './Components/loggedInCategories';
-import CreateAccount from "./Components/CreateAccount";
-import TestFrameEditor from './Components/TestFrameEditor';
-import NewContentPage from './Pages/NewContentPage';
-import LoginPage from './Pages/LoginPage';
-
-<<<<<<< HEAD
 import Menubar from './Components/Menubar'
 import Userinfo from "./Components/Userinfo"
 import LoggedOutCategories from './Components/loggedOutCategories'
 import LoggedInCategories from './Components/loggedInCategories'
 import LoginForm from "./Components/LoginForm"
 import CreateAccount from "./Components/CreateAccount"
-=======
->>>>>>> b274081a7172ee72d4ebb51915c8b22ae7cc5b3a
 class App extends Component
 {
-    constructor(props)
-    {
-        super(props)
-        this.state={
-            page         : 'homepage',
-            loggedIn     : false,
-            bio          : '',
-            username     : '',
-            userInfoError: false
-        }
-        this.changePage=this.changePage.bind(this)
-        this.login     =this.login.bind(this)
-        this.refresh   =this.refresh.bind(this)
-    }
+	constructor(props)
+	{
+		super(props)
+		this.state={
+			page         : 'homepage',
+			loggedIn     : false,
+			bio          : '',
+			username     : '',
+			userInfoError: false
+		}
+		this.changePage=this.changePage.bind(this)
+		this.login     =this.login.bind(this)
+		this.refresh   =this.refresh.bind(this)
+	}
 
-    componentDidMount()
-    {
-        this.refresh()
-    }
+	componentDidMount()
+	{
+		this.refresh()
+	}
 
-    changePage(page, data)
-    {
-        this.setState({page: page})
-        if (page === 'homepage') {
-            this.refresh()
-        }
-    }
+	changePage(page)
+	{
+		this.setState({page: page})
+		this.refresh()
+	}
 
-    async refresh()
-    {
-        let res=await fetch('/generalBase/id')
-        res    = await res.json()
-        this.setState({loggedIn: res.id.length!==0})
+	async refresh()
+	{
+		let res=await fetch('/generalBase/id')
+		res    = await res.json()
+		this.setState({loggedIn: res.id.length!==0})
 
-        let userInfoRes = await fetch('/user/info');
-        userInfoRes = await userInfoRes.json();
-        if (userInfoRes.status === 'OK') {
-            let userInfo = userInfoRes.content;
-            this.setState({
-                bio: userInfo.bio,
-                username: userInfo.username,
-                userInfoError: false
-            });
-        } else if (userInfoRes.status === 403) {
-            // user is not authorized to make the request (probably logged out)
-            this.setState({userInfoError: true})
-        }
-        else
-        {
-            this.setState({userInfoError: true})
-        }
-    }
+		let userInfoRes=await fetch('/user/info')
+		userInfoRes    = await userInfoRes.json()
+		if(userInfoRes.status==='OK')
+		{
+			let userInfo=userInfoRes.content
+			this.setState({bio: userInfo.bio, username: userInfo.username})
+		}
+		else if(userInfoRes.status===403)
+		{
+			// user is not authorized to make the request (probably logged out)
+			this.setState({userInfoError: true})
+		}
+		else
+		{
+			this.setState({userInfoError: true})
+		}
+	}
 
-    login()
-    {
-        this.setState({loggedIn: true})
-    }
+	login()
+	{
+		this.setState({loggedIn: true})
+	}
 
-<<<<<<< HEAD
 	render()
 	{
 		const pages={
@@ -96,50 +80,32 @@ class App extends Component
 				/>,
 			login   : <LoginForm changePage={this.changePage} login={this.login}/>
 		}
-=======
-    render()
-    {
-        const pages={
-            create  : <CreateAccount changePage={this.changePage} />,
-            homepage: this.state.loggedIn ? <LoggedInCategories/> : <LoggedOutCategories/>,
-            userInfo:
-                <UserInfo
-                    bio={this.state.bio}
-                    username={this.state.username}
-                    profilePictureUrl="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201804/RTX5L0IT.jpeg?qlnshqvD6xOuLhFcVvAqQ3OzqMM9ncYQ"
-                    error={this.state.userInfoError}
-                />,
-            login   : <LoginPage changePage={this.changePage} login={this.login} />,
-            editor  : <TestFrameEditor />,
-            newContent: <NewContentPage changePage={this.changePage} />
-        }
->>>>>>> b274081a7172ee72d4ebb51915c8b22ae7cc5b3a
 
-        return (
-            <>
-                <link
-                    rel="stylesheet"
-                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossOrigin="anonymous"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-                    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-                    crossOrigin="anonymous"
-                />
-                <div className="App">
-                    <Menubar loggedIn={this.state.loggedIn} changePage={this.changePage} />
-                    {pages[this.state.page]}
-                </div>
-            </>
-        )
-    }
+		return (
+			<>
+				<link
+					rel="stylesheet"
+					href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+					integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+					crossOrigin="anonymous"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+					integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+					crossOrigin="anonymous"
+				/>
+				<div className="App">
+					<Menubar loggedIn={this.state.loggedIn} changePage={this.changePage}/>
+					{pages[this.state.page]}
+				</div>
+			</>
+		)
+	}
 }
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('react')
+	<App/>,
+	document.getElementById('react')
 )
 

cfb18ca98c0a93fc6d381b677283d34545daf5ea
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-19 01:52:29 -0400

    Likes.js

--- src/main/js/Components/Likes.js
+++ src/main/js/Components/Likes.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Heart from './Heart'
-export default function()
+export default function({generalBaseId='Stub', likedByCurrentUserColor='red', notLikedByCurrentUserColor='black'})
 {
-	return <div style={{backgroundColor:"Green",height:"300px",width:"300px"}}>
+	const [likedByCurrentUser, setLikedByCurrentUser]=useState(true)//This should fetch whether a user likedByCurrentUser this or not
+	console.assert(likedByCurrentUser!==undefined&&Object.getPrototypeOf(likedByCurrentUser)===Boolean.prototype)//Type checking that likedByCurrentUser is boolean
+	return <div style={{
+		backgroundColor: "Green",
+		height         : "300px",
+		width          : "300px"
+	}}>
 		<Heart/>
 	</div>
 }

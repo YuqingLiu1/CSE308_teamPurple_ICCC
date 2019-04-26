e3c48a207732b0661184a5aded34435ec96a17fe
Yuqing <yuqing.liu@stonybrook.edu>
2019-04-26 03:50:34 -0400

    Add function to list all user items

---
+++ src/main/java/com/teampurple/iccc/models/UserList.java
@@ -0,0 +1,16 @@
+package com.teampurple.iccc.models;
+
+import java.util.List;
+
+public class UserList {
+    private List<UserItem> userlist;
+
+    public List<UserItem> getUserlist() {
+        return userlist;
+    }
+
+    public void setUserlist(List<UserItem> userlist) {
+        this.userlist = userlist;
+    }
+
+}

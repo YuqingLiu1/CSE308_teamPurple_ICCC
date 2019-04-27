673c427c10f88760f4b0aaa25cc8f0cf0dd3e763
Elliot Tuck <eat3774@gmail.com>
2019-04-27 19:36:56 -0400

    Start adding documentation to UserAccountController

--- src/main/java/com/teampurple/iccc/controllers/UserAccountController.java
+++ src/main/java/com/teampurple/iccc/controllers/UserAccountController.java
@@ -68,6 +68,52 @@ public class UserAccountController {
     // @GetMapping("/general/info")
 
 
+    /**
+     * Description:
+     *   - get information about current logged in user
+     *   - must be logged in
+     *
+     * Request params:
+     *   - none
+     *
+     * Returns:
+     *   - status: String ('OK' or 'error')
+     *   - content (if status is 'OK'):
+     *       {
+     *           username: String (the username of the current logged in user),
+     *           bio: String (the bio of the current logged in user),
+     *           email: String (the email of the current logged in user),
+     *           password: String (the hashed password of the current logged in user),
+     *           sketchRef: String (Sketch ID of the sketch associated with the current logged in user),
+     *           userCategories: [ array of Category IDs for categories on the user's account page ],
+     *           homeCategories: [ array of Category IDs for categories on the user's home page ],
+     *           user:
+     *             {
+     *                 id: String (User ID of the current logged in user),
+     *                 generalBaseRef: String (GeneralBase ID of the current logged in user),
+     *                 email: String (email of the current logged in user),
+     *                 password: String (hashed password of the current loggedd in user),
+     *                 liked: [ array of GeneralBase IDs of things the current logged in user has liked ],
+     *                 content: [ array of ContentBase IDs of content created by the current logged in user ],
+     *                 userCategories: [ array of Category IDs for categories on the user's account page ],
+     *                 homeCategories: [ array of Category IDs for categories on the user's home page ]
+     *             },
+     *           generalBase:
+     *             {
+     *                 id: String (GeneralBase ID of the current logged in user),
+     *                 typeRef: String (User ID of the current logged in user),
+     *                 type: String ("User'),
+     *                 sketch: String (Sketch ID of the current logged in user's profile picture sketch),
+     *                 title: String (username of the current logged in user),
+     *                 description: String (bio of the current logged in user),
+     *                 dateCreated: String (ISO 8601 datetime of when the user account was created),
+     *                 dateLastEdited: String (ISO 8601 datetime of when the user account was last edited),
+     *                 children: [ array of ContentBase IDs of the current logged in user's series ],
+     *                 likers: [ array of User IDs of users who have liked the current logged in user ],
+     *                 comments: [ array of Comment IDs of the current logged in user's comments ]
+     *             }
+     *       }
+     */
     @GetMapping("/user/info")
     public Response getUserInfo() {
         User currentUser = auth.getCurrentUser();
@@ -99,6 +145,25 @@ public class UserAccountController {
         return new Response(Response.OK, userInfo);
     }
 
+    // TODO: handle updating categories, liked content/users, and comments
+    // TODO: change the dateLastEdited when making updates
+    /**
+     * Description:
+     *   - edit information about the current logged in user
+     *   - must be logged in
+     *   - the logged in user can only edit their own information
+     *
+     * Request params:
+     *   - username: String (new username of the current logged in user; optional)
+     *   - bio: String (new bio of the current logged in user; optional),
+     *   - email: String (new email of the current logged in user; optional)
+     *     - note that emails uniquely identify users, so the current logged in user's email cannot be changed to
+     *       an email address that matches that of another existing user
+     *   - password: String (new plaintext password of the current logged in user; optional)
+     *
+     * Returns:
+     *   - status: String ('OK' or 'error')
+     */
     @PostMapping("/user/edit")
     public Response editUserInfo(@RequestBody UserInfo userInfo) {
         User currentUser = auth.getCurrentUser();
@@ -112,7 +177,6 @@ public class UserAccountController {
         String password = userInfo.getPassword();
         String email = userInfo.getEmail();
         String bio = userInfo.getBio();
-        String sketchRef = userInfo.getSketchRef();
         if (username != null) {
             currentGeneralBase.setTitle(username);
         }
@@ -125,27 +189,37 @@ public class UserAccountController {
         if (bio != null) {
             currentGeneralBase.setDescription(bio);
         }
-        if (sketchRef != null) {
-            currentGeneralBase.setSketch(sketchRef);
-        }
 
         // don't forget to save changes to database
         try {
             generalbase.save(currentGeneralBase);
             users.save(currentUser);
         } catch (MongoException ex) {
-            return new Response("error");
+            return new Response(Response.ERROR);
         }
 
-        return new Response("OK");
+        return new Response(Response.OK);
     }
 
     // TODO: create a new sketch for the new user's profile picture upon creation
+    /**
+     * Description:
+     *   - create a new user
+     *   - provided email must be unique among existing users' emails in the database
+     *
+     * Request params:
+     *   - username: String (username of the new user to create)
+     *   - password: String (plaintext password of the new user to create),
+     *   - email: String (email of the new user to create)
+     *
+     * Returns:
+     *   - status: String ('OK' or 'error')
+     */
     @PostMapping(value = "/user/add")
     public Response addUserAccount(@RequestBody NewUser user) {
         try {
             GeneralBase gb = new GeneralBase();
-            gb.setType("User");
+            gb.setType(GeneralBase.USER_TYPE);
             gb.setTitle(user.getUsername());
             gb.setDescription("Hello! I am " + user.getUsername() + ".");
             gb.setDateCreated(new Date());
@@ -166,7 +240,16 @@ public class UserAccountController {
         }
     }
 
-
+    /**
+     * Description:
+     *   - check if a user with a given email address exists
+     *
+     * Request params:
+     *   - email: String (email of user we are searching for)
+     *
+     * Returns:
+     *   - status: String ('OK' or 'error')
+     */
     @PostMapping("/user/exists")
     public Response checkUserExist(@RequestBody UserInfo userInfo) {
         if (!users.existsByEmail(userInfo.getEmail())) {

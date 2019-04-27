d765d2c370967e1fc55731488dc9412e4e367c35
Elliot Tuck <eat3774@gmail.com>
2019-04-27 17:02:55 -0400

    Add documentation to HomeController

--- src/main/java/com/teampurple/iccc/controllers/HomeController.java
+++ src/main/java/com/teampurple/iccc/controllers/HomeController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    /**
+     * Description:
+     *   - get the main HTML page of the application
+     *
+     * Request params:
+     *   - none
+     *
+     * Returns:
+     *   - the HTML page containing the root div for React to hook into
+     */
     @GetMapping("/")
     public String getHomePage() {
         return "index";

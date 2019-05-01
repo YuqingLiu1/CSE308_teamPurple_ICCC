d2187f069c6d17d09628050cccf3c1b854723020
Haoyu-Tan <haotan@cs.stonybrook.edu>
2019-04-30 23:05:24 -0400

    fix the problem of create account
    input box are centered

--- src/main/js/Components/CreateAccount.js
+++ src/main/js/Components/CreateAccount.js
@@ -46,22 +46,22 @@ class CreateAccount extends Component{
         <div className="form-group createDiv">
             <label className="createLabel">User Name</label>
         <br/>
-        <input type="text" className="form-control createInput" value={this.state.username} placeholder="e.g:user123" onChange={(e) => {this.setState({username: e.target.value})}}/>
+        <input type="text" className="createInput" value={this.state.username} placeholder="e.g:user123" onChange={(e) => {this.setState({username: e.target.value})}}/>
         </div>
         <div className="form-group createDiv">
             <label className="createLabel">Email</label>
             <br></br>
-            <input type="email" className="form-control createInput" value={this.state.email} placeholder="e.g:user123@mail.com" onChange={(e) => {this.setState({email: e.target.value})}}/>
+            <input type="email" className="createInput" value={this.state.email} placeholder="e.g:user123@mail.com" onChange={(e) => {this.setState({email: e.target.value})}}/>
         </div>
         <div className="form-group createDiv">
             <label className="createLabel">Password</label>
             <br/>
-            <input type="password" className="form-control createInput" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}}/>
+            <input type="password" className="createInput" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}}/>
         </div>
         <div className="form-group createDiv">
             <label className="createLabel">Enter password again</label>
         <br/>
-        <input type="password" className="form-control createInput" value={this.state.passwordAgain} onChange={(e) => {this.setState({passwordAgain: e.target.value})}}/>
+        <input type="password" className="createInput" value={this.state.passwordAgain} onChange={(e) => {this.setState({passwordAgain: e.target.value})}}/>
         </div>
         <br/>
         <button type="submit" className="btn btn-primary createButton" style={{"textAlign":"center"}} onClick={this.handleSubmit}>create</button>

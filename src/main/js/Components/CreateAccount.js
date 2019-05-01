876886e46c273940bb5cc801a4102e80d17d9222
Haoyu-Tan <haotan@cs.stonybrook.edu>
2019-04-30 22:32:06 -0400

    fix problem of entering password

--- src/main/js/Components/CreateAccount.js
+++ src/main/js/Components/CreateAccount.js
@@ -9,6 +9,7 @@ class CreateAccount extends Component{
             username: '',
             email: '',
             password: '',
+            passwordAgain: ''
 
         }
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,53 +18,57 @@ class CreateAccount extends Component{
 
     async handleSubmit(event) {
         event.preventDefault();
-
-        let res = await fetch('/user/add', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(this.state)
-        });
-        res = await res.json();
-        if (res.status === 'OK') {
-            this.props.changePage('homepage')
-        } else {
-            alert('Could not create new account')
+        if (this.state.password !== this.state.passwordAgain) {
+            alert("Password does not match with each other!");
+        }
+        else{
+            let res = await fetch('/user/add', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(this.state)
+            });
+            res = await res.json();
+            if (res.status === 'OK') {
+                this.props.changePage('homepage')
+            } else {
+                alert('Could not create new account')
+            }
         }
     }
 
     render(){
         return(
             <div className="form2">
-                <form action="/user/add" method="POST">
-                    <h3 style={{"textAlign":"center", "color":"black", "fontFamily":"Comic Sans MS, cursive, sans-serif"}}>Create Your Account</h3>
-                    <div className="form-group createDiv">
-                        <label className="createLabel">User Name</label>
-                        <br/>
-                        <input type="text" className="form-control createInput" value={this.state.username} placeholder="e.g:user123" onChange={(e) => {this.setState({username: e.target.value})}}/>
-                    </div>
-                    <div className="form-group createDiv">
-                        <label className="createLabel">Email</label>
-                        <br></br>
-                        <input type="email" className="form-control createInput" value={this.state.email} placeholder="e.g:user123@mail.com" onChange={(e) => {this.setState({email: e.target.value})}}/>
-                    </div>
-                    <div className="form-group createDiv">
-                        <label className="createLabel">Password</label>
-                        <br/>
-                        <input type="password" className="form-control createInput" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}}/>
-                    </div>
-                    <div className="form-group createDiv">
-                        <label className="createLabel">Enter password again</label>
-                        <br/>
-                        <input type="password" className="form-control createInput" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}}/>
-                    </div>
-                    <br/>
-                    <button type="submit" className="btn btn-primary createButton" style={{"textAlign":"center"}} onClick={this.handleSubmit}>create</button>
+            <form action="/user/add" method="POST">
+            <h3 style={{"textAlign":"center", "color":"black", "fontFamily":"Comic Sans MS, cursive, sans-serif"}}>Create Your Account</h3>
+        <div className="form-group createDiv">
+            <label className="createLabel">User Name</label>
+        <br/>
+        <input type="text" className="form-control createInput" value={this.state.username} placeholder="e.g:user123" onChange={(e) => {this.setState({username: e.target.value})}}/>
+        </div>
+        <div className="form-group createDiv">
+            <label className="createLabel">Email</label>
+            <br></br>
+            <input type="email" className="form-control createInput" value={this.state.email} placeholder="e.g:user123@mail.com" onChange={(e) => {this.setState({email: e.target.value})}}/>
+        </div>
+        <div className="form-group createDiv">
+            <label className="createLabel">Password</label>
+            <br/>
+            <input type="password" className="form-control createInput" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}}/>
+        </div>
+        <div className="form-group createDiv">
+            <label className="createLabel">Enter password again</label>
+        <br/>
+        <input type="password" className="form-control createInput" value={this.state.passwordAgain} onChange={(e) => {this.setState({passwordAgain: e.target.value})}}/>
+        </div>
+        <br/>
+        <button type="submit" className="btn btn-primary createButton" style={{"textAlign":"center"}} onClick={this.handleSubmit}>create</button>
 
-                </form>
+            </form>
             </div>
-        );
+    );
     }
 }
 

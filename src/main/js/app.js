b9d595857e53948fc8683f3161ea18623cafa42d
Elliot Tuck <eat3774@gmail.com>
2019-04-30 21:12:57 -0400

    Change profilePictureUrl to point to port 80

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -99,7 +99,7 @@ class App extends Component
 					bio={this.state.bio}
 					username={this.state.username}
 					// profilePictureUrl={'http://35.227.70.138/generalBase/thumbnail?id=' + this.state.generalBaseId}
-					profilePictureUrl={'http://localhost:8080/generalBase/thumbnail?id=' + this.state.generalBaseId}
+					profilePictureUrl={'http://localhost:80/generalBase/thumbnail?id=' + this.state.generalBaseId}
 					error={this.state.userInfoError}
 					changePage={this.changePage}
 				/>,

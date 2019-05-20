80390c70288414a9b7a0344fa3008b773c76b29a
Elliot Tuck <eat3774@gmail.com>
2019-05-19 21:23:33 -0400

    Fix bug in ChangePassword
    Fix bug where password and password2 could be different. Now they must
    be the same before you can change your password.

--- src/main/js/Components/ChangePassword.js
+++ src/main/js/Components/ChangePassword.js
@@ -1,63 +1,75 @@
-import React, {Component} from 'react'
-import doFetch from '../Helpers/general.js'
+require('@babel/polyfill')
 
-class ChangePassword extends Component
-{
-	constructor(props)
-	{
-		super(props)
+import React, { useState } from 'react';
 
-		this.state={
-			password      : '',
-			passwordAgain : '',
-			badCredentials: false,
-			errors        : false
+import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
+
+export default function ChangePassword({ }) {
+	const [password, setPassword] = useState('');
+	const [password2, setPassword2] = useState('');
+	const [passwordMismatch, setPasswordMismatch] = useState(false);
+
+	async function handleSubmit(event) {
+		try {
+			event.preventDefault();
+
+			// make sure password and password2 match
+			if (password !== password2) {
+				setPasswordMismatch(true);
+				return;
+			} else {
+				setPasswordMismatch(false);
+			}
+
+			let res = await fetch('/user/edit', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					password: password
+				})
+			});
+			res = await res.json();
+			if (res.status !== 'OK') throw new Error('Failed to change password.');
+		} catch (err) {
+			console.error(err);
 		}
 	}
 
-	handlePassword = (event) => {
-		this.setState({password : event.target.value})
-	}
-
-	handlePass = (event) => {
-		this.setState({passwordAgain : event.target.value})
-	}
-
-	handleSubmit = async (event) => {
-		event.preventDefault()
-		console.log("password is: " + this.state.password);
-		console.log("password again is: " + this.state.passwordAgain);
-
-		
-		console.log("in sending password");
-		let result = await doFetch('/user/setpassword', {method: 'POST', body: this.state.password})
-		console.log(result);
-	}
-
-	render()
-	{
-		return (
+	// rendering logic
+	return (
+		<Container>
 			<div className="form1">
 				<form>
 					<h3 style={{textAlign: "center", color: "black"}}>Change Password</h3>
+					{
+						passwordMismatch &&
+						<Alert variant='danger'>Password do not match</Alert>
+					}
 					<div className="changePasswordDiv">
 						<label className="changePasswordLabel">Password</label>
 						<br/>
-						<input type="password" className="form-control" className="changePasswordInput" onChange={this.handlePassword}/>
+						<input
+							type="password"
+							className="form-control changePasswordInput"
+							onChange={(e) => {setPassword(e.target.value)}}/>
 					</div>
 					<div className="editDiv">
 						<label className="changePasswordLabel">Enter Password Again</label>
 						<br/>
-						<input type="password" className="form-control" className="changePasswordInput" onChange={this.handlePass}/>
+						<input
+							type="password"
+							className="form-control changePasswordInput"
+							onChange={(e) => {setPassword2(e.target.value)}}/>
 					</div>
 					<br/>
-					<button type="submit" className="btn btn-primary" className="changePasswordBtn" onClick={this.handleSubmit}>
+					<button type="submit" className="btn btn-primary" className="changePasswordBtn" onClick={handleSubmit}>
 						change
 					</button>
 				</form>
 			</div>
-		)
-	}
+		</Container>
+	);
 }
-
-export default ChangePassword

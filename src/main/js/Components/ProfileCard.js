2db553c487bcfce64bd515e8fac46eff4667c689
RyanBurgert219 <sqrtryan@gmail.com>
2019-05-21 17:07:46 -0400

    Style

--- src/main/js/Components/ProfileCard.js
+++ src/main/js/Components/ProfileCard.js
@@ -2,27 +2,35 @@ import React from 'react';
 import Image from "react-bootstrap/Image";
 import DBAwareEdiText from "./DBAwareEdiText";
 import Likes from './Likes'
+import Col from 'react-bootstrap/Col'
+
+
 
 export default function ProfileCard({ generalBaseId, profileThumbnailUrl, username, editable, loggedInUserId }) {
+    let userTitleText=<div style={{display: 'inline-block'}}>
+        {
+            username &&
+            (
+                editable ?
+                    <DBAwareEdiText
+                        viewProps={{style:{fontSize:50}}}
+                        type="text"
+                        name="username"
+                        value={username}
+                    />
+                    :
+                    <p>{username}</p>
+            )
+        }
+    </div>
+    let userImage    =<Image src={profileThumbnailUrl} rounded fluid className="mb-3"/>
     return <div>
         <div style={{textAlign: "center"}}>
-            <Image src={profileThumbnailUrl} rounded fluid className="mb-3"/>
-            <div style={{display: 'inline-block'}}>
-                {
-                    username &&
-                    (
-                        editable ?
-                            <DBAwareEdiText
-                                type="text"
-                                name="username"
-                                value={username}
-                            />
-                                :
-                            <p>{username}</p>
-                    )
-                }
-            </div>
+            <Col style={{textAlign: "center",display: 'inline-block'}}>
+                {userTitleText}
+                {userImage}
+                <Likes generalBaseId={generalBaseId} loggedInUserId={loggedInUserId}/>
+            </Col>
         </div>
-        <Likes generalBaseId={generalBaseId} loggedInUserId={loggedInUserId}/>
     </div>
 }

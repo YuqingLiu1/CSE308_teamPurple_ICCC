45e0950e495c3d41519d4fdc1e4b4c2d496ce124
Haoyu-Tan <haotan@cs.stonybrook.edu>
2019-05-20 22:46:17 -0400

    add view comment(backend) by passing a user's general base id
    backend

--- src/main/java/com/teampurple/iccc/repositories/CommentRepository.java
+++ src/main/java/com/teampurple/iccc/repositories/CommentRepository.java
@@ -3,5 +3,9 @@ package com.teampurple.iccc.repositories;
 import com.teampurple.iccc.models.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
+
 public interface CommentRepository extends MongoRepository<Comment, String>{
+    List<Comment> findByAuthor(String comment);
 }
